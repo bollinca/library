b45c5fcd72c7085b519d0e938665c9510ec270a9
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-07 11:47:06 -0400

    Draft grid layout and render function

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ function Book(title, author, pages, readStatus) {
     this.pages = pages;
     this.readStatus = readStatus;
 
-    this.info = [title, author, pages, readStatus]
+    this.info = [title, author, pages, readStatus];
 }
 
 Book.prototype.addBook = function addBookToLibrary() {
     myLibrary.push(this.info);
+}
+
+const cardContainer = document.querySelector('#card-container');
+function render(book) {
+    let newCard = document.createElement('div');
+    newCard.classList.add('card');
+    newCard.textContent = book.info;
+    cardContainer.appendChild(newCard);
 }
