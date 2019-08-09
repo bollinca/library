9b48d200ebb999e759890c57ed918603c1be0304
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-09 14:03:20 -0400

    Modify render function, add render all

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-let myLibrary = [];
+let myLibrary = [['hello', 'test', 123, 'no'], [1, 2, 3, 4], [5, 1, 6, 2], [52, 15, 15, 61]];
+
+function renderAll(library) {
+    for (let i = 0; i < library.length; i++) {
+        render(library[i]);
+    }
+}
+
+function render(book) {
+    let newCard = document.createElement('div');
+    let cardTitle = document.createElement('h2')
+    let cardAuthor = document.createElement('p')
+    let cardPages = document.createElement('p')
+    let cardStatus = document.createElement('p')
+    newCard.classList.add('card');
+    newCard.appendChild(cardTitle);
+    newCard.appendChild(cardAuthor);
+    newCard.appendChild(cardPages);
+    newCard.appendChild(cardStatus);
+    cardTitle.textContent = book[0];
+    cardAuthor.textContent = book[1];
+    cardPages.textContent = book[2];
+    cardStatus.textContent = book[3];
+    cardContainer.appendChild(newCard);
+}
 
 function Book(title, author, pages, readStatus) {
     this.title = title;
@@ -14,9 +38,5 @@ Book.prototype.addBook = function addBookToLibrary() {
 }
 
 const cardContainer = document.querySelector('#card-container');
-function render(book) {
-    let newCard = document.createElement('div');
-    newCard.classList.add('card');
-    newCard.textContent = book.join(' ');
-    cardContainer.appendChild(newCard);
-}
+
+renderAll(myLibrary);
