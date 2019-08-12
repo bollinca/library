ca5b1c369673599bf6840c246fb1481069d7cf2f
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-12 11:51:16 -0400

    Add check for identical books

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function Book(title, author, pages, readStatus) {
 }
 
 Book.prototype.addBook = function addBookToLibrary() {
-    myLibrary.push(this.info);
+    myLibrary.push(this);
 }
 
 function renderAll(library) {
@@ -25,27 +25,39 @@ function render(book) {
     let cardAuthor = document.createElement('p');
     let cardPages = document.createElement('p');
     let cardStatus = document.createElement('p');
-    let exitButton = document.createElement('button');
+    let deleteButton = document.createElement('button');
     newCard.appendChild(cardTitle);
     newCard.appendChild(cardAuthor);
     newCard.appendChild(cardPages);
     newCard.appendChild(cardStatus);
-    newCard.appendChild(exitButton);
+    newCard.appendChild(deleteButton);
 
     cardTitle.textContent = book.title;
     cardAuthor.textContent = `Author: ${book.author}`;
     cardPages.textContent = `Page Count: ${book.pages}`;
     cardStatus.textContent = `Finished?: ${book.readStatus}`;
-    exitButton.textContent = 'X';
+    deleteButton.textContent = 'X';
     
     newCard.classList.add('card');
-    exitButton.classList.add('exit');
+    deleteButton.classList.add('delete');
     cardContainer.appendChild(newCard);
+    //deleteButton.addEventListener('click', () => removeBook());
+    if (!(myLibrary.some(eachBook => areIdentical(book, eachBook)))) {
+        book.addBook();
+    } else {
+        console.log('Books are identical, will not be added');
+    }
+}
+
+function areIdentical(book1, book2) {
+    if (book1.info.toString() === book2.info.toString()) {
+        return true
+    }
+return false
 }
 
 function submitForm() {
     const newBook = new Book(bookForm[0].value, bookForm[1].value, bookForm[2].value, findCheckedRadio());
-    console.log(typeof newBook);
     render(newBook);
     clearForm();
 }
