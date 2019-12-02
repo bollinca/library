703758980cf49345559bcef1a4714a0e1b2df119
bollinca <bollinca522@gmail.com>
2019-12-02 10:10:38 -0500

    Update to conform to JS Standard linter

--- script.js
+++ script.js
@@ -1,202 +1,202 @@
 class Book {
-    constructor(title, author, pages, readStatus){
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.readStatus = readStatus;
-        this.info = [this.title, this.author, this.pages, this.readStatus];
-    }
-    updateInfo() {
-        this.info = [this.title, this.author, this.pages, this.readStatus];
-    }
-    addBook() {
-        myLibrary.push(this);
-        updateStorage();
-    }
-    toggleRead(e) {
-        if (this.readStatus === 'no') {
-            this.readStatus = 'yes';
-        } else {
-            this.readStatus = 'no';
-        }
-        e.target.classList.toggle('read');
-        this.updateInfo();
-        updateStorage();
-    }
-}
+  constructor (title, author, pages, readStatus) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.readStatus = readStatus
+    this.info = [this.title, this.author, this.pages, this.readStatus]
+  }
 
-function alwaysRender(book) {
-    let newCard = document.createElement('div');
-    newCard.setAttribute('data-index', myLibrary.indexOf(book));
-    newCard.classList.add('card');
+  updateInfo () {
+    this.info = [this.title, this.author, this.pages, this.readStatus]
+  }
 
-    let cardTitle = document.createElement('h2');
-    cardTitle.textContent = book.title;
+  addBook () {
+    myLibrary.push(this)
+    updateStorage()
+  }
 
-    let cardAuthor = document.createElement('p');
-    cardAuthor.textContent = `Author: ${book.author}`;
-
-    let cardPages = document.createElement('p');
-    cardPages.textContent = `Page Count: ${book.pages}`;
-
-    let cardStatus = document.createElement('button');
-    cardStatus.setAttribute('data-index', myLibrary.indexOf(book));
-    cardStatus.classList.add('toggle')
-    if (book.readStatus === 'yes') {
-        cardStatus.classList.add('read');
-    }
-    cardStatus.addEventListener('click', (e) => (myLibrary[e.target.attributes['data-index'].value]).toggleRead(e));
-
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = 'X';
-    deleteButton.setAttribute('data-index', myLibrary.indexOf(book));
-    deleteButton.classList.add('delete');
-    deleteButton.addEventListener('click', (e) => removeBook(e));
-
-    newCard.appendChild(cardTitle);
-    newCard.appendChild(cardAuthor);
-    newCard.appendChild(cardPages);
-    newCard.appendChild(cardStatus);
-    newCard.appendChild(deleteButton);
-    cardContainer.appendChild(newCard);
-}
-
-function checkPages(pageNumber) {
-    pageNumber = Number(pageNumber);
-    if (pageNumber !== 0) {
-        return ((pageNumber <= 20000 && pageNumber > 0
-            || (pageNumber === NaN)))
-    }
-    else {
-        return false;
-    }
-}
-
-function renderAll(library) {
-    for (let i = 0; i < library.length; i++) {
-        alwaysRender(library[i]);
-    }
-}
-
-function renderNew(book) {
-    if (!(myLibrary.some(eachBook => areIdentical(book, eachBook)))) {
-        book.addBook();
-        alwaysRender(book);
+  toggleRead (e) {
+    if (this.readStatus === 'no') {
+      this.readStatus = 'yes'
     } else {
-        alert('Books are identical, will not be added');
+      this.readStatus = 'no'
     }
+    e.target.classList.toggle('read')
+    this.updateInfo()
+    updateStorage()
+  }
 }
 
-function areIdentical(book1, book2) {
-    if (book1.info.toString() === book2.info.toString()) {
-        return true;
+function alwaysRender (book) {
+  const newCard = document.createElement('div')
+  newCard.setAttribute('data-index', myLibrary.indexOf(book))
+  newCard.classList.add('card')
+
+  const cardTitle = document.createElement('h2')
+  cardTitle.textContent = book.title
+
+  const cardAuthor = document.createElement('p')
+  cardAuthor.textContent = `Author: ${book.author}`
+
+  const cardPages = document.createElement('p')
+  cardPages.textContent = `Page Count: ${book.pages}`
+
+  const cardStatus = document.createElement('button')
+  cardStatus.setAttribute('data-index', myLibrary.indexOf(book))
+  cardStatus.classList.add('toggle')
+  if (book.readStatus === 'yes') {
+    cardStatus.classList.add('read')
+  }
+  cardStatus.addEventListener('click', (e) => (myLibrary[e.target.attributes['data-index'].value]).toggleRead(e))
+
+  const deleteButton = document.createElement('button')
+  deleteButton.textContent = 'X'
+  deleteButton.setAttribute('data-index', myLibrary.indexOf(book))
+  deleteButton.classList.add('delete')
+  deleteButton.addEventListener('click', (e) => removeBook(e))
+
+  newCard.appendChild(cardTitle)
+  newCard.appendChild(cardAuthor)
+  newCard.appendChild(cardPages)
+  newCard.appendChild(cardStatus)
+  newCard.appendChild(deleteButton)
+  cardContainer.appendChild(newCard)
+}
+
+function checkPages (pageNumber) {
+  pageNumber = Number(pageNumber)
+  if (pageNumber !== 0 && (pageNumber <= 20000 && pageNumber > 0) && !(isNaN(pageNumber))) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function renderAll (library) {
+  for (let i = 0; i < library.length; i++) {
+    alwaysRender(library[i])
+  }
+}
+
+function renderNew (book) {
+  if (!(myLibrary.some(eachBook => areIdentical(book, eachBook)))) {
+    book.addBook()
+    alwaysRender(book)
+  } else {
+    window.alert('Books are identical, will not be added')
+  }
+}
+
+function areIdentical (book1, book2) {
+  if (book1.info.toString() === book2.info.toString()) {
+    return true
+  }
+  return false
+}
+
+function submitForm () {
+  const newBook = new Book(bookForm[0].value.trim(), bookForm[1].value.trim(), bookForm[2].value.trim(), findCheckedRadio())
+  if (!bookForm[0].value.trim()) {
+    window.alert('Please enter a valid title')
+    clearForm()
+    return
+  }
+  if (checkPages(bookForm[2].value) === true) {
+    renderNew(newBook)
+    clearForm()
+  } else {
+    window.alert('Invalid page number submitted')
+    clearForm()
+  }
+}
+
+function findCheckedRadio () {
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+      return radioButtons[i].value
     }
-    return false;
+  }
 }
 
+function clearForm () {
+  bookForm[0].value = ''
+  bookForm[1].value = ''
+  bookForm[2].value = ''
+  bookForm.status.value = 'yes'
+  toggleForm()
+}
 
-function submitForm() {
-    const newBook = new Book(bookForm[0].value.trim(), bookForm[1].value.trim(), bookForm[2].value.trim(), findCheckedRadio());
-    if (!bookForm[0].value.trim()) {
-        alert('Please enter a valid title');
-        clearForm();
-        return;
+function toggleForm () {
+  bookForm.classList.toggle('invisible')
+}
+
+function removeBook (e) {
+  let deleteConfirm
+  const deleteSelector = e.target.attributes['data-index'].value
+  const allBooks = Array.from(document.querySelectorAll('.card'))
+  for (let i = 0; i < allBooks.length; i++) {
+    if (allBooks[i].attributes['data-index'].value === deleteSelector) {
+      deleteConfirm = allBooks[i].attributes['data-index'].value
     }
-    if (checkPages(bookForm[2].value) === true) {
-        renderNew(newBook);
-        clearForm();
-    } else {
-        alert('Invalid page number submitted');
-        clearForm();
+    if (deleteConfirm === deleteSelector) {
+      myLibrary.splice(i, 1);
+      (allBooks[i].parentNode.removeChild(allBooks[i]))
+      break
     }
+  }
+  updateStorage()
+  removeAll()
+  renderAll(myLibrary)
 }
 
-function findCheckedRadio() {
-    for (let i = 0; i < radioButtons.length; i++) {
-        if (radioButtons[i].checked) {
-            return radioButtons[i].value;
-        }
-    }
+function removeAll () {
+  const deletionTotal = cardContainer.childNodes.length
+  for (let i = 0; i < deletionTotal; i++) {
+    cardContainer.removeChild(cardContainer.childNodes[0])
+  }
 }
 
-function clearForm() {
-    bookForm[0].value = '';
-    bookForm[1].value = '';
-    bookForm[2].value = '';
-    bookForm.status.value = 'yes';
-    toggleForm();
+function checkStorage () {
+  if (window.localStorage.storedLibrary && window.localStorage.storedLibrary.length > 1) {
+    return 1
+  } else {
+    return 0
+  }
 }
 
-function toggleForm() {
-    bookForm.classList.toggle('invisible');
+function updateStorage () {
+  window.localStorage.setItem('storedLibrary', JSON.stringify(myLibrary))
 }
 
-function removeBook(e) {
-    let deleteConfirm;
-    let deleteSelector = e.target.attributes['data-index'].value;
-    let allBooks = Array.from(document.querySelectorAll('.card'));
-    for (let i = 0; i < allBooks.length; i++) {
-        if (allBooks[i].attributes['data-index'].value === deleteSelector) {
-            deleteConfirm = allBooks[i].attributes['data-index'].value;
-        }
-        if (deleteConfirm === deleteSelector) {
-            myLibrary.splice(i, 1);
-            (allBooks[i].parentNode.removeChild(allBooks[i]));
-            break;
-        }
-    }
-    updateStorage();
-    removeAll();
-    renderAll(myLibrary);
+function clearStorage () {
+  const message = 'Click "OK" to revert entire library to default, demonstration books.'
+  if (window.confirm(message) === true) {
+    window.localStorage.clear()
+    removeAll()
+    myLibrary = [new Book('Demo', 'J.K. Fakerson', 243, 'no'), new Book('A photo a day', 'Mr Lenz de Kamera', 365, 'yes')]
+    renderAll(myLibrary)
+  }
 }
 
-function removeAll() {
-    let deletionTotal = cardContainer.childNodes.length;
-    for (let i = 0; i < deletionTotal; i++) {
-        cardContainer.removeChild(cardContainer.childNodes[0]);
-    }
-}
+let myLibrary = []
+const bookForm = document.querySelector('#new-book-form')
+const radioButtons = document.querySelectorAll('.radio')
+const newBookButton = Array.from(document.querySelectorAll('.new-book-button'))
+const cardContainer = document.querySelector('#card-container')
+const submitButton = document.querySelector('#add-button')
+const storageClearButton = document.querySelector('#storage-button')
 
-function checkStorage() {
-    if (localStorage.storedLibrary && localStorage.storedLibrary.length > 1) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-function updateStorage() {
-    localStorage.setItem('storedLibrary', JSON.stringify(myLibrary));
-}
-
-function clearStorage() {
-    let message = 'Click "OK" to revert entire library to default, demonstration books.'
-    if (confirm(message) === true) {
-        localStorage.clear();
-        removeAll();
-        myLibrary = [new Book('Demo', 'J.K. Fakerson', 243, 'no'), new Book('A photo a day', 'Mr Lenz de Kamera', 365, 'yes')];
-        renderAll(myLibrary);
-    }
-}
-
-let myLibrary = [];
-let bookForm = document.querySelector('#new-book-form');
-const radioButtons = document.querySelectorAll('.radio');
-const newBookButton = Array.from(document.querySelectorAll('.new-book-button'));
-const cardContainer = document.querySelector('#card-container');
-const submitButton = document.querySelector('#add-button');
-const storageClearButton = document.querySelector('#storage-button');
-
-newBookButton.forEach(button => button.addEventListener('click', toggleForm));
-submitButton.addEventListener('click', () => submitForm());
-storageClearButton.addEventListener('click', () => clearStorage());
+newBookButton.forEach(button => button.addEventListener('click', toggleForm))
+submitButton.addEventListener('click', () => submitForm())
+storageClearButton.addEventListener('click', () => clearStorage())
 
 if (checkStorage() === 0) {
-    myLibrary = [new Book('Demo', 'J.K. Fakerson', 243, 'no'), new Book('A photo a day', 'Mr Lenz de Kamera', 365, 'yes')];
+  myLibrary = [new Book('Demo', 'J.K. Fakerson', 243, 'no'), new Book('A photo a day', 'Mr Lenz de Kamera', 365, 'yes')]
 } else if (checkStorage() === 1) {
-    myLibrary = JSON.parse(localStorage.getItem('storedLibrary'));
-    let tempLibrary = myLibrary.map(book => Object.values(book));
-    myLibrary = tempLibrary.map(book => new Book(...book));
+  myLibrary = JSON.parse(window.localStorage.getItem('storedLibrary'))
+  const tempLibrary = myLibrary.map(book => Object.values(book))
+  myLibrary = tempLibrary.map(book => new Book(...book))
 }
 
-renderAll(myLibrary);
+renderAll(myLibrary)
